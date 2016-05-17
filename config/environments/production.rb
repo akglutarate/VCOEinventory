7e31153c00937d71008c9c5a05b425150df19776
akglutarate <akglutarate@gmail.com>
2016-05-17 07:55:29 -0700

    email pw fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     :port => "587",
     :domain => "gmail.com",
 		:user_name => "vcoe.inventory@gmail.com",
-		:password => "VCEducation",
+		:password => "VCoe.invent!",
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
