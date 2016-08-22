7bfffcd7a7cf11b3bfb25eedb89f678289e0479f
akglutarate <akglutarate@gmail.com>
2016-08-21 17:53:50 -0700

    update smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,11 +46,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    :port => "587",
+    :port => 587,
     :domain => "gmail.com",
 		:user_name => "vcoe.inventory@gmail.com",
-		:password => "VCoe.invent!",
-    :authentication => "plain",
+		:password => "VcOe.Invent!",
+    :authentication => :plain,
     :enable_starttls_auto => true
   } 
 	
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,11 +85,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    :port => "587",
+    :port => 587,
     :domain => "gmail.com",
 		:user_name => "vcoe.inventory@gmail.com",
-		:password => "VCoe.invent!",
-    :authentication => "login",
+		:password => "VcOe.Invent!",
+    :authentication => :plain,
     :enable_starttls_auto => true
   } 
 	
